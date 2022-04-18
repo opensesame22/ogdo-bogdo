41b593e4df1aea95d8ebded46915e6abaa333c93
opensesame22 <103602375+opensesame22@users.noreply.github.com>
2022-04-18 11:43:50 -0600

    Add files via upload

---
+++ The People.py
@@ -0,0 +1,14 @@
+import pygame, sys
+
+pygame.init()
+clock = pygame.time.Clock()
+
+WIDTH = 600
+HEIGHT = 300
+display_surface = pygame.display.set_mode((WIDTH,HEIGHT))
+
+
+if __name__ = "__main__"   
+
+
+pygame.quit()
